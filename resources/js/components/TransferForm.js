9a658c2fc5d87bbcb5e4b296617a213e80b3f076
Claudio Junior Familia Morel <claudio.familia.morel@gmail.com>
2019-09-23 16:27:53 -0400

    Adding low validation on form

--- resources/js/components/TransferForm.js
+++ resources/js/components/TransferForm.js
@@ -4,12 +4,12 @@ const TransferForm = props => (
     <form onSubmit={props.onSumit} className="row">
         <div className="input-field col-sm-12 col-md-4">    
             <i className="material-icons prefix">local_offer</i>                    
-            <input placeholder="Type a description" onChange={props.onChangeDescription} value={props.descriptionInput} id="description" type="text" className="validate" />
+            <input placeholder="Type a description" required onChange={props.onChangeDescription} value={props.descriptionInput} id="description" type="text" className="validate" />
             <label htmlFor="description">Description</label>
         </div>
         <div className="input-field col-sm-12 col-md-4">  
             <i className="material-icons prefix">attach_money</i>                                          
-            <input placeholder="Type the amount" onChange={props.onChangeAmount} value={props.amountInput} id="amount" type="number" className="validate" />
+            <input placeholder="Type the amount" required onChange={props.onChangeAmount} value={props.amountInput} id="amount" type="number" className="validate" />
             <label htmlFor="amount">Amount</label>
         </div> 
         <div className="col-sm-12 col-md-4 text-white text-left">  
